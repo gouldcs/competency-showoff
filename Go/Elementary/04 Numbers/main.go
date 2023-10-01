bca81a5dc8cf1ab7b7a2601be6d39ce11e3d6559
gouldcs <cam@gouldcs.com>
2023-09-30 21:54:36 -0700

    Go elementaryt problem 6

--- Go/Elementary/04 Numbers/main.go
+++ Go/Elementary/04 Numbers/main.go
@@ -16,13 +16,14 @@ func applyStrategy() {
 	fmt.Println("Select an option:")
 	fmt.Println("(1) Sum all numbers")
 	fmt.Println("(2) Sum all numbers divisible by 3 or 5 only")
+	fmt.Println("(3) Compute product of all numbers")
 	var isValidInput bool = false
 
 	for !isValidInput {
-		fmt.Print("Which do you select (1, 2)? ")
+		fmt.Print("Which do you select (1, 2, 3)? ")
 		_, err := fmt.Fscanf(r, "%d\n", &input)
 
-		isValidInput = err == nil && (input == 1 || input == 2)
+		isValidInput = err == nil && (input >= 1 && input <= 3)
 
 		if !isValidInput {
 			fmt.Println("That is not a valid option.")
@@ -36,9 +37,12 @@ func applyStrategy() {
 	if input == 1 {
 		sum = calculateSum(limit)
 		prettyPrintResult(limit, sum)
-	} else {
+	} else if input == 2 {
 		sum = calculateSumThreeOrFiveOnly(limit)
 		prettyPrintResultThreeOrFiveOnly(limit, sum)
+	} else {
+		var product uint64 = calculateProduct(limit)
+		prettyPrintProduct(limit, product)
 	}
 }
 
@@ -71,6 +75,16 @@ func calculateSumThreeOrFiveOnly(limit int) int {
 	return sum
 }
 
+func calculateProduct(limit int) uint64 {
+	var product uint64 = 1
+
+	for i := uint64(1); i <= uint64(limit); i++ {
+		product = product * i
+	}
+
+	return product
+}
+
 func prettyPrintResult(limit int, sum int) {
 	for i := 1; i <= limit; i++ {
 		if i == 1 {
@@ -95,6 +109,18 @@ func prettyPrintResultThreeOrFiveOnly(limit int, sum int) {
 	}
 
 	fmt.Printf("= %d", sum)
+}
+
+func prettyPrintProduct(limit int, product uint64) {
+	for i := 1; i <= limit; i++ {
+		if i == 1 {
+			fmt.Printf("%d ", i)
+		} else {
+			fmt.Printf("x %d ", i)
+		}
+	}
+
+	fmt.Printf("= %d", product)
 }
 
 func isDivisibleByThreeOrFive(number int) bool {
