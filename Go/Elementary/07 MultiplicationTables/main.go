d0e16ca227e5513e555a72e409e928778afac396
gouldcs <cam@gouldcs.com>
2023-10-01 08:52:47 -0700

    [Go] Finished Problem 07 Elementary

--- Go/Elementary/07 MultiplicationTables/main.go
+++ Go/Elementary/07 MultiplicationTables/main.go
@@ -1,22 +1,8 @@
 package main
 
-import "fmt"
-
-/*
-* | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 |
-1 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 |
-2 | 2 | 4 | ...
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-*/
+import (
+	"fmt"
+)
 
 func main() {
 	var multiplicationTable [12][12]int = generateTable()
@@ -40,41 +26,24 @@ func printTable(multiplicationTable [12][12]int) {
 	for i := 0; i < 12; i++ {
 		for j := 0; j < 12; j++ {
 			currentNumber := multiplicationTable[i][j]
-			currentMax := (j + 1) * 12
-
-			var spacing string
-
-			if currentMax < 10 {
-				spacing = "   "
-			} else if currentMax < 100 {
-				spacing = "  "
-			} else {
-				spacing = " "
-			}
 			if j == 0 {
-				if currentNumber < 10 {
-					fmt.Printf(" %d  |", currentNumber)
-				} else {
-					fmt.Printf(" %d |", currentNumber)
-				}
+				fmt.Printf("%d\t| %d", currentNumber, currentNumber)
+			} else {
+				fmt.Printf(", %d", currentNumber)
 			}
 
-			fmt.Printf("%s%d%s|", spacing, currentNumber, spacing)
 		}
 		fmt.Print("\n")
 	}
 }
 
 func printHeader() {
-	fmt.Print(" *  |")
+	fmt.Print("*\t|")
 	for i := 1; i < 13; i++ {
-		var currentValueMax = i * 12
-		if currentValueMax >= 100 {
-			fmt.Printf("   %d   |", i)
-		} else if currentValueMax >= 10 {
-			fmt.Printf("  %d  |", i)
+		if i == 1 {
+			fmt.Printf(" %d", i)
 		} else {
-			fmt.Printf(" %d |", i)
+			fmt.Printf(", %d", i)
 		}
 	}
 	fmt.Print("\n")
