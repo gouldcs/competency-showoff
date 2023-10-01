8985de46da985ecf07d0e0f664eddcf590f648e6
gouldcs <cam@gouldcs.com>
2023-09-30 18:40:42 -0700

    Numbers challenge advanced

--- Go/Elementary/04 Numbers/main.go
+++ Go/Elementary/04 Numbers/main.go
@@ -1,11 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
+	applyStrategy()
+}
+
+func applyStrategy() {
+	var input int
+	r := bufio.NewReader(os.Stdin)
+	fmt.Println("Select an option:")
+	fmt.Println("(1) Sum all numbers")
+	fmt.Println("(2) Sum all numbers divisible by 3 or 5 only")
+	var isValidInput bool = false
+
+	for !isValidInput {
+		fmt.Print("Which do you select (1, 2)? ")
+		_, err := fmt.Fscanf(r, "%d\n", &input)
+
+		isValidInput = err == nil && (input == 1 || input == 2)
+
+		if !isValidInput {
+			fmt.Println("That is not a valid option.")
+			r.ReadBytes(10)
+		}
+	}
+
 	var limit int = acceptInput()
-	var sum int = calculateSum(limit)
-	prettyPrintResult(limit, sum)
+	var sum int
+
+	if input == 1 {
+		sum = calculateSum(limit)
+		prettyPrintResult(limit, sum)
+	} else {
+		sum = calculateSumThreeOrFiveOnly(limit)
+		prettyPrintResultThreeOrFiveOnly(limit, sum)
+	}
 }
 
 func acceptInput() int {
@@ -25,6 +59,18 @@ func calculateSum(limit int) int {
 	return sum
 }
 
+func calculateSumThreeOrFiveOnly(limit int) int {
+	var sum int
+
+	for i := 1; i <= limit; i++ {
+		if isDivisibleByThreeOrFive(i) {
+			sum += i
+		}
+	}
+
+	return sum
+}
+
 func prettyPrintResult(limit int, sum int) {
 	for i := 1; i <= limit; i++ {
 		if i == 1 {
@@ -35,4 +81,22 @@ func prettyPrintResult(limit int, sum int) {
 	}
 
 	fmt.Printf("= %d", sum)
+}
+
+func prettyPrintResultThreeOrFiveOnly(limit int, sum int) {
+	for i := 1; i <= limit; i++ {
+		if isDivisibleByThreeOrFive(i) {
+			if i == 3 {
+				fmt.Printf("%d ", i)
+			} else {
+				fmt.Printf("+ %d ", i)
+			}
+		}
+	}
+
+	fmt.Printf("= %d", sum)
+}
+
+func isDivisibleByThreeOrFive(number int) bool {
+	return number%3 == 0 || number%5 == 0
 }
