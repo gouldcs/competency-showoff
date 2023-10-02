cf749d4ed559c245d9d170fb59f793ac82dbf3d7
gouldcs <cam@gouldcs.com>
2023-10-01 18:50:24 -0700

    [Go] Finishes elementary problem 11. Graduated!!

---
+++ Go/Elementary/11 Alternating Series/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	const ONE_MILLION = 1000000
+	var sum float64 = 0.0
+
+	for k := 2; k <= ONE_MILLION; k++ {
+		sum += math.Pow(-1.0, float64(k)+1.0) / (2.0 * (float64(k) - 1.0))
+	}
+
+	fmt.Printf("The sum of  ((-1)^{k+1})/(2 * k-1) where 1 >= k <= 1,000,000 is %f\n", sum)
+	fmt.Printf("This, multiplied by 4, is %f", sum*4)
+
+}
